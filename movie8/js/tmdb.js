631bfee79ef54e02bfbdd215ee17a05a833abf9a
agustiawan260 <62092066+agustiawan260@users.noreply.github.com>
2021-11-15 13:00:22 +0700

    Backup movie8 first commit

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -1,3 +1,31 @@
 const key = 'c0a1ae6487c5e5f3be8dce4c8cca4789';
 const endpoint_url = 'https://api.themoviedb.org/3/';
 
+function getListMovie() {
+    fetch(endpoint_url + "movie/now_playing?api_key=" + key + "&language=en-US&page=1")
+    .then(status)
+    .then(json)
+    .then(function(data) {
+
+        let movieHTML = "";
+        data.results.forEach(function(movie) {
+        movieHTML += `
+            <div class="col m3 s6">
+                <div class="card">
+                    <a href="./movie.html?id=${movie.id}">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
+                        </div>
+                    </a>
+                    <div class="card-content text-center">
+                        <strong>${movie.title}</strong>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+
+    document.getElementById("movie_list").innerHTML = movieHTML;
+    })
+    .catch(error);
+}
