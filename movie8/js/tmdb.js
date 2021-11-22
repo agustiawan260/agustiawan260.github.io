a551df2afc58aa82902d16e38d8aed231901a9ad
agustiawan260 <62092066+agustiawan260@users.noreply.github.com>
2021-11-22 23:52:06 +0700

    Backup praktikum 08 movie 8

--- movie8/js/tmdb.js
+++ movie8/js/tmdb.js
@@ -1,31 +1,39 @@
-const key = 'c0a1ae6487c5e5f3be8dce4c8cca4789';
+const key = 'c0a1ae6487c5e5f3be8dce4c8cca4789'; 
 const endpoint_url = 'https://api.themoviedb.org/3/';
 
-function getListMovie() {
-    fetch(endpoint_url + "movie/now_playing?api_key=" + key + "&language=en-US&page=1")
+
+function getListMovie(services, sectiontitle) {
+    // fetch untuk dapat data dari server API penyedia
+    fetch(endpoint_url + services + "?api_key=" + key + "&language=en-US&page=1")
     .then(status)
     .then(json)
     .then(function(data) {
+    // Objek/array JavaScript dari response.json() masuk lewat data.
+    // Menyusun komponen card movie secara dinamis
 
-        let movieHTML = "";
-        data.results.forEach(function(movie) {
-        movieHTML += `
-            <div class="col m3 s6">
-                <div class="card">
-                    <a href="./movie.html?id=${movie.id}">
-                        <div class="card-image waves-effect waves-block waves-light">
-                            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
-                        </div>
-                    </a>
-                    <div class="card-content text-center">
-                        <strong>${movie.title}</strong>
+    var moviesHTML = "";
+    data.results.forEach(function(movie) {
+    moviesHTML =  moviesHTML + `
+        <div class="col m3 s6">
+            <div class="card moviecard">
+                <a href="./movie.html?id=${movie.id}">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
                     </div>
+                </a>
+                <div class="card-content text-center">
+                    <strong>${movie.title}</strong><br>
+                    <strong>Release Date : ${movie.release_date}</strong>
                 </div>
-            </div>
-            `;
-        });
 
-    document.getElementById("movie_list").innerHTML = movieHTML;
+            </div>
+        </div>
+        `;
+    });
+
+    document.getElementById("movie_list").innerHTML = moviesHTML;
+    document.getElementById("section_title").innerHTML = sectiontitle;
+
     })
     .catch(error);
 }
